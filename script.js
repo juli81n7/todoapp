e4a676a25561819de0832e6cd755b67a9cc8afe6
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-09-14 15:34:29 +0200

    push tilbage

--- script.js
+++ script.js
@@ -112,16 +112,16 @@ function createNewListBtn(name) {
   cloneBtn.querySelector("button").classList.add("active");
   document.querySelector(".btn_list_inner").appendChild(cloneBtn);
 
-  cloneBtn.querySelector("button").addEventListener("click", (e) => {
-    // Remove the "active" class from all buttons
-    document.querySelectorAll(".btn_list_inner button").forEach((btn) => {
-      btn.classList.remove("active");
-    });
+  // cloneBtn.querySelector("button").addEventListener("click", (e) => {
+  //   // Remove the "active" class from all buttons
+  //   document.querySelectorAll(".btn_list_inner button").forEach((btn) => {
+  //     btn.classList.remove("active");
+  //   });
 
-    // Add the "active" class to the clicked button
-    e.target.classList.add("active");
-    showNewList(name);
-  });
+  //   // Add the "active" class to the clicked button
+  //   e.target.classList.add("active");
+  // });
+
   showNewList(name);
 }
 
